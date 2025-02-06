b859f24fa8f8e05382a14e93c83b8750e238b993
shinsky5 <ohhanel2219@naver.com>
2025-02-06 09:09:00 +0900

    Create LoginController

---
+++ src/main/java/com/sky_aml/aml/login/LoginController.java
@@ -0,0 +1,10 @@
+package com.sky_aml.aml.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+    public void Test() {
+        System.out.println("HI");
+    }
+}
